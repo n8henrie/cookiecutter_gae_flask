53785c0662cdeb3dc8f8aae640a5c471d5ec2ba7
Nathan Henrie <n8henrie@gmail.com>
2014-11-13 10:49:33 -0700

    Re-add remainder of tests, manage.py

--- {{cookiecutter.app_name}}/tests/test_config.py
+++ {{cookiecutter.app_name}}/tests/test_config.py
@@ -1,7 +1,17 @@
-import sys
+# -*- coding: utf-8 -*-
+from {{cookiecutter.app_name}}.app import create_app
+from {{cookiecutter.app_name}}.settings import ProdConfig, DevConfig
 
-sys.path.append('/path/to/gae_pathfix/dir/')
-import gae_pathfix
+def test_production_config():
+    app = create_app(ProdConfig)
+    assert app.config['ENV'] == 'prod'
+    assert app.config['DEBUG'] is False
+    assert app.config['DEBUG_TB_ENABLED'] is False
+    assert app.config['ASSETS_DEBUG'] is False
 
-from {{cookiecutter.app_name}} import app
-print(app.config['ALLOWED_EXTENSIONS'])
+
+def test_dev_config():
+    app = create_app(DevConfig)
+    assert app.config['ENV'] == 'dev'
+    assert app.config['DEBUG'] is True
+    assert app.config['ASSETS_DEBUG'] is True
